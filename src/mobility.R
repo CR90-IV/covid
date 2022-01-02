4b567b114cd52969fbaef59f32a704f9f69697e5
CR90-IV <43439686+CR90-IV@users.noreply.github.com>
2022-01-02 12:13:40 +0000

    Upload mobility

--- src/mobility.R
+++ src/mobility.R
@@ -6,6 +6,8 @@ library(gghighlight)
 library(zoo)
 library(ggthemes)
 library(scales)
+library(lubridate)
+library(RColorBrewer)
 
 get_data_from_gov_api <- function(request_url) {
   res = GET(request_url)
@@ -32,11 +34,11 @@ plot <- GB %>%
     values_to="change_from_baseline"
   ) %>%
   mutate_at("place_category", str_replace, "_percent_change_from_baseline", "") %>%
+  mutate(place_category = gsub("_", " ", place_category)) %>%
   mutate(change_from_baseline = change_from_baseline/100)
 
 latest_date <- max(plot$date)
 
-plot %>% filter(sub_region_1 == "") %>%
 
 ggplot(aes(x=date, y=change_from_baseline, color=place_category)) +
   geom_line(
@@ -86,3 +88,105 @@ ggplot(aes(x=date, y=change_from_baseline, color=place_category)) +
   )
 
 ggsave(paste("plots/community_mobility.png", sep=""), width=10,height=8)
+
+
+calendar <- plot %>%
+  
+  mutate(
+    wday = wday(date, week_start = 1, label=TRUE),
+    day = day(date),
+    month = month(date, label=TRUE),
+    week = strftime(date, format = "%W"),#if_else(isoweek(date)<=52, isoweek(date), isoweek(date)-52),
+    year = year(date),
+    year_month = format(date, "%Y-%m")
+    ) %>%
+  
+  mutate(
+    place_category = str_wrap(place_category, 12),
+    change_factor = cut(
+      change_from_baseline,
+      breaks=c(-1, -0.75, -0.5, -0.1, 0.1, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2),
+      #labels=c("-100% to -50%", "-50% to 0%", "0% to 50%", "50% to 100%", "100% to 150%", "150% to 200%")
+      )
+  ) %>%
+  
+  filter(
+    #date > as.Date("2021-01-01"),
+    #date < as.Date("2021-12-30"),
+    sub_region_1 == "",
+  )
+  
+
+
+newPalette <- c(
+  #rev(brewer.pal(3, "Oranges")),
+  "#FDAE6B","#FDD0A2", "#FEEDDE",
+  "grey90",
+  brewer.pal(7, "Greens"))
+
+
+ggplot(calendar, aes(x=week, y=wday)) +
+  
+  geom_tile(
+    aes(fill=change_factor),
+    color="white",
+    size=0.25
+    ) +
+  
+  scale_fill_manual(
+    values=newPalette,
+    drop=FALSE
+    ) +
+  
+  # scale_fill_gradient2(
+  #   low="red",
+  #   mid="grey90",
+  #   high="green",
+  #   #mid="#F0F0F0"
+  #   #limits = c(-1, 1)
+  #   )+
+  
+  #geom_text(aes(label=day))+
+  
+  facet_grid(
+    cols=vars(year_month),
+    rows=vars(place_category),
+    scales="free",
+    space="free",
+    switch = "y"
+    ) +
+  
+  theme_fivethirtyeight() +
+  
+  scale_y_discrete(limits = rev)+
+  
+  labs(
+    x="", y="",
+    title="Are people going outside?",
+    subtitle="Community mobility in the UK compared to Jan-Feb 2020 baseline",
+    caption=paste(
+      "Data: Google COVID-19 Community Mobility Reports\nProduced",
+      Sys.time(),
+      "with most recent data up to",
+      latest_date
+    )
+  )+
+  
+  theme(
+    legend.title=element_blank(),
+    panel.grid=element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border=element_blank(),
+    strip.background=element_blank(),
+    legend.position="top",
+    legend.justification="left",
+    legend.direction="horizontal",
+    axis.text.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.text.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    rect = element_rect(fill = "white")
+    ) 
+
+ggsave("plots/mobility_over_time.png", width=17, height=8)    
+
