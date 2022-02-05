279dbfd4634d6dbcbd399d5b92fb45888f31226a
CR90-IV <43439686+CR90-IV@users.noreply.github.com>
2022-02-05 23:03:04 +0000

    Create boosters vs prevalence

---
+++ src/01_prevalence/prevalence_vs_boosters.R
@@ -0,0 +1,89 @@
+source("src/01_prevalence/prevalence_core.R")
+
+# Import data
+
+data_vax = get_data_from_gov_api(
+  paste(
+    "https://api.coronavirus.data.gov.uk/v2/data?areaType=utla",
+    "&metric=cumVaccinationFirstDoseUptakeByVaccinationDatePercentage",
+    "&metric=cumVaccinationSecondDoseUptakeByVaccinationDatePercentage",
+    "&metric=cumVaccinationThirdInjectionUptakeByVaccinationDatePercentage",
+    "&format=csv",
+    sep="")
+)
+
+
+data_utla = get_data_from_gov_api(
+  paste(
+    "https://api.coronavirus.data.gov.uk/v2/data?areaType=utla",
+    "&metric=newCasesBySpecimenDate",
+    "&metric=newVirusTestsBySpecimenDate",
+    "&format=csv",
+    sep="")
+)
+
+# Arrange data
+
+vax <- data_vax %>%
+  rename(
+    dose_1_uptake = cumVaccinationFirstDoseUptakeByVaccinationDatePercentage,
+    dose_2_uptake = cumVaccinationSecondDoseUptakeByVaccinationDatePercentage,
+    dose_3_uptake = cumVaccinationThirdInjectionUptakeByVaccinationDatePercentage
+  ) %>%
+  mutate(
+    dose_1_uptake = dose_1_uptake/100,
+    dose_2_uptake = dose_2_uptake/100,
+    dose_3_uptake = dose_3_uptake/100
+  )
+
+
+utla <- get_adjusted_prevalence(data_utla)
+
+utla_peak <- utla %>%
+  group_by(areaName) %>%
+  filter(date > "2021-11-01") %>%
+  slice_max(date, n=1) %>%
+  mutate(vax_date = as.Date(date) - 10) %>%
+  merge(y=vax, by.x=c("vax_date", "areaName"), by.y=c("date", "areaName"))
+
+latest_date <- max(utla$date)
+
+
+
+color_code = "#0070C0"
+
+ggplot(utla_peak, aes(x=dose_3_uptake, y=prevalenceRate)) +
+
+  geom_point(
+    size=3,
+    color=color_code,
+  ) +
+  
+  scale_x_continuous(labels = scales::percent) +
+  scale_y_continuous(labels = scales::percent) +
+  
+  theme_fivethirtyeight() +
+  
+  theme(
+    text = element_text(family = "IBM Plex Sans"),
+    panel.grid.minor = element_blank(),
+    axis.line.x = element_line(color="#C0C0C0"),
+    axis.ticks = element_blank(),          #strip axis ticks
+    strip.text.x = element_text(hjust=1, face="bold", size=rel(1)),
+    rect = element_rect(fill = "#FFFFFF"),
+  ) +
+  
+  labs(
+    title="Omicron prevalence compared with vaccine coverage",
+    subtitle="Using adjustments to compensate for testing delays and underreporting",
+    x="Vaccine coverage",
+    caption=paste(
+      "Data: UKHSA and ONS (via coronavirus.data.gov.uk) | Adjustment method: microCOVID Project\nProduced",
+      Sys.time(),
+      "with most recent data up to",
+      latest_date
+    )
+  )
+
+ggsave(paste("plots/prevalence_by_utla.png", sep=""), width=10,height=10)
+
